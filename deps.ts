4e04ef65fb1fedf78a890426e8520f772b6ca06c
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-27 00:11:36 +0900

    chore(deps): add deps

---
+++ deps.ts
@@ -0,0 +1,22 @@
+// Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export {
+  type Handler,
+  type Middleware,
+} from "https://deno.land/x/http_middleware@1.0.0/mod.ts";
+export { timingSafeEqual } from "https://deno.land/std@0.184.0/crypto/timing_safe_equal.ts";
+export { Status } from "https://deno.land/std@0.184.0/http/http_status.ts";
+export { isString } from "https://deno.land/x/isx@1.3.1/is_string.ts";
+export { isBoolean } from "https://deno.land/x/isx@1.3.1/is_boolean.ts";
+export { isUndefined } from "https://deno.land/x/isx@1.3.1/is_undefined.ts";
+export { isNull } from "https://deno.land/x/isx@1.3.1/is_null.ts";
+export { AuthenticationHeader } from "https://deno.land/x/http_utils@1.0.0/header.ts";
+export {
+  type AuthParams,
+  parseAuthorization,
+  stringifyAuthorization as stringifyChallenge,
+  type Token68,
+} from "https://deno.land/x/authorization_parser@1.0.0-beta.2/mod.ts";
+export { isErr, unsafe } from "https://deno.land/x/result_js@1.0.0/mod.ts";
+export { toHashString } from "https://deno.land/std@0.184.0/crypto/to_hash_string.ts";
